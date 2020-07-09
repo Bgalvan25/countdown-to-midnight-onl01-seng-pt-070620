d994dc5659d25101ceefd1a86d88ff5f02b13a73
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-09 23:26:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 def countdown(seconds)
   while seconds > 0  
   puts "#{seconds} SECOND(S)!"
-  second -= 1
+  seconds -= 1
 end
 "HAPPY NEW YEAR!"
 end 
 
 def countdown_with_sleep(seconds)
 while seconds > 0 
-puts "#{second} SECOND(S)!"
+puts "#{seconds} SECOND(S)!"
 sleep(1)
 seconds -= 1 
 end 
